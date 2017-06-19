af21cabca855ddd5635d5be456fe4e8affacd319
ranatomar <ranatomar21@gmail.com>
2017-06-19 10:21:39 +0530

    Add the dashboard

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero'
+
+export const HEROES:Hero[]=[
+  {id:11,name:"first hero"},
+  {id:12,name:"second hero"},
+   {id:13,name:"13 hero"},
+  {id:14,name:"14 hero"},
+   {id:15,name:"15 hero"},
+  {id:16,name:"16 hero"},
+   {id:17,name:"17 hero"},
+  {id:18,name:"18 hero"}
+];
